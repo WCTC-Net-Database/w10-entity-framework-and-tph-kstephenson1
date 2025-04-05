c7d70a9d49291518c467e30b9f1cc6dc3de8fcf6
Kyle S <kstephenson1@my.wctc.edu>
2025-04-05 00:05:47 -0500

    Fixed MaxUses in items table

---
+++ Models/Interfaces/IItem.cs
@@ -0,0 +1,11 @@
+﻿using w10_assignment_ksteph.Models.Inventories;
+
+namespace w10_assignment_ksteph.Models.Interfaces;
+
+public interface IItem
+{
+    // Interface tha allows items to exist.
+    public string Name { get; set; }
+    public string Description { get; set; }
+    Inventory Inventory { get; set; }
+}
---
+++ Models/Items/ConsumableItems/ItemBook.cs
@@ -0,0 +1,33 @@
+﻿using w10_assignment_ksteph.Models.Interfaces.ItemBehaviors;
+using w10_assignment_ksteph.Services.DataHelpers;
+
+namespace w10_assignment_ksteph.Models.Items.ConsumableItems;
+
+public class ItemBook : ConsumableItem, IConsumableItem
+{
+    public override string ItemType { get; set; } = "ItemBook";
+    public override int MaxUses { get; set ; } = 10;
+    public ItemBook()
+    {
+        Name = "Book";
+        Description = "Use to read the book.";
+        MaxUses = 10;
+        UsesLeft = MaxUses;
+    }
+
+    public ItemBook(string name, string desc) : base(name, desc)
+    {
+        MaxUses = 10;
+        UsesLeft = MaxUses;
+    }
+
+    public void UseItem()
+    {
+        Console.WriteLine($"You read the book. Isn't there a battle going on right now!?");
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} ({UsesLeft}/{MaxUses})";
+    }
+}
