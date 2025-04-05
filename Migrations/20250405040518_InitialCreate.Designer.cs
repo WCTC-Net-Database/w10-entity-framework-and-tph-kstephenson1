58cfb51d33ff6410757d52d7ba03c65b52ce4c85
Kyle S <kstephenson1@my.wctc.edu>
2025-04-05 00:05:56 -0500

    Migrations

--- Migrations/20250405040518_InitialCreate.Designer.cs
+++ Migrations/20250405040518_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using W9_assignment_template.Data;
 
@@ -10,9 +11,11 @@ using W9_assignment_template.Data;
 namespace w10_assignment_ksteph.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20250405040518_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
